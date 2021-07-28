75ea839a020b08ed259a38b86993d073092c10f5
kostya7771 <mnyh2019@gmail.com>
2021-07-29 00:39:31 +0300

    added section category, video popup, fix some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,10 @@
-$(function name() {
+$(function() {
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 3000
-
-    
   });
+  var mixer = mixitup('.category-content');
 })
