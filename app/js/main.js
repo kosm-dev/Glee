c0c3efa71468b62a309a51518bb73ba32b86f7c6
kostya7771 <mnyh2019@gmail.com>
2021-08-16 00:30:53 +0300

    added shop page and shop-product-2 page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function() {
+  $('.shop-product__addbtn').on('click', function () {
+    $('.shop-product__boxcart').toggleClass('shop-product__boxlink--active');
+    $(this).removeClass('shop-product__boxlink--active');
+  });
+
+  // range slider
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.01,
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
   
+
+  // slick slider
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -7,9 +28,29 @@ $(function() {
     autoplay: true,
     autoplaySpeed: 3000
   });
+ 
+
+  // rateyo
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "8px",
+  });
+
+  $(".product-star").rateYo({
+    starWidth: "15px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "8px",
+  });
 
   
+  
 
+  // mixit up
   var containerEl1 = document.querySelector('[data-ref="products-1"]');
   var containerEl2 = document.querySelector('[data-ref="category-1"]');
 
@@ -22,9 +63,7 @@ $(function() {
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
   
-
-
-
+  
 
 
 
